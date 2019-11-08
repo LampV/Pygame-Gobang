d17175186a47dd826beaf6c7ba624552704a71b8
LampV <1071952403@qq.com>
2019-11-08 17:03:57 +0800

    将代码抽象为函数

--- CheeseGame.py
+++ CheeseGame.py
@@ -12,38 +12,99 @@ import pygame
 from sys import exit
 from pygame.locals import *
 
-if __name__ == '__main__':
+
+def human_action(env):
+    """获取人类落子动作"""
+    while True:
+        _action = None
+        for event in pygame.event.get():  # 这是pygame接受输入的循环，直到退出或者落子为止
+            # 点击叉时退出
+            if event.type == QUIT:
+                exit()
+            # 按下Esc时退出
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    exit()
+            # 如果按下鼠标，检查落子，判断游戏是否结束
+            if event.type == MOUSEBUTTONDOWN:
+                x, y = event.pos
+                # 将点击位置转换为棋盘上的点
+                row, col = env.point_convert(x, y)
+                _action = (row, col)
+                if _action == (-1, -1) or env.has_piece(_action):
+                    _action = None
+                break
+        if _action:
+            return _action
+
+
+def ai_vs_ai():
     cheese_border_size = 15
-    env = CheeseENV(border_count=cheese_border_size, line_margin=40)
-    agent = CheeseDQN(cheese_border_size * cheese_border_size, cheese_border_size * cheese_border_size)
+    env = CheeseENV(enable_pygame=False, border_count=cheese_border_size, line_margin=40)
+    black_agent = CheeseDQN(cheese_border_size * cheese_border_size, cheese_border_size * cheese_border_size, 'blackAgent')
+    white_agent = CheeseDQN(cheese_border_size * cheese_border_size, cheese_border_size * cheese_border_size, 'whiteAgent')
     while True:  # 这是维持游戏循环进行的循环
+        print('new game', flush=True)
         state = env.reset()  # 每局游戏开始之前重置环境
+        step = 0
         while True:  # 这是每一局游戏内部接受落子的循环
-            action = None
             if env.get_color() == 1:  # 黑棋，则由人输入
-                for event in pygame.event.get():  # 这是pygame接受输入的循环，直到退出或者落子为止
-                    # 点击叉时退出
-                    if event.type == QUIT:
-                        exit()
-                    # 按下Esc时退出
-                    if event.type == KEYDOWN:
-                        if event.key == K_ESCAPE:
-                            exit()
-                    # 如果按下鼠标，检查落子，判断游戏是否结束
-                    if event.type == MOUSEBUTTONDOWN:
-                        x, y = event.pos
-                        # 将点击位置转换为棋盘上的点
-                        row, col = env.point_convert(x, y)
-                        action = (row, col)
-                        if action == (-1, -1) or env.has_piece(action):
-                            action = None
-                        break
+                action = black_agent.get_action(state)
+                action = (action // cheese_border_size, action % cheese_border_size)
             else:  # 白棋，则由agent输入
-                action = agent.get_action(state)
+                action = white_agent.get_action(state)
                 action = (action // cheese_border_size, action % cheese_border_size)
 
-            if action:
-                next_state, reward, done, _ = env.step(action)
-                if done:
-                    break
-                state = next_state
+            if not action:  # 若是无效action，跳过
+                raise TypeError("不应该出现无效action!")
+            # 模型训练及保存
+            step += 1
+            next_state, reward, done, _ = env.step(action)
+            action = action[0] * cheese_border_size + action[1]
+            if done:
+                print(f'''{'black' if env.get_color() ==1 else 'white'} player win!''', flush=True)
+                print('game end, save model.')
+                black_agent.save()
+                white_agent.save()
+                break
+            black_agent.train(state, next_state, action, reward)  # 黑棋的原则是reward最大化，黑棋赢的时候reward是1，输的时候reward是-1
+            white_agent.train(state, next_state, action, -reward)  # 白棋刚好相反
+            state = next_state
+            if step % 20 == 0:
+                print('save model per 20 steps.')
+                black_agent.save()
+                white_agent.save()
+
+
+def human_vs_ai():
+    cheese_border_size = 15
+    env = CheeseENV(border_count=cheese_border_size, line_margin=40)
+    white_agent = CheeseDQN(cheese_border_size * cheese_border_size, cheese_border_size * cheese_border_size, 'whiteAgent')
+    while True:  # 这是维持游戏循环进行的循环
+        print('new game', flush=True)
+        state = env.reset()  # 每局游戏开始之前重置环境
+        step = 0
+        while True:  # 这是每一局游戏内部接受落子的循环
+            if env.get_color() == 1:  # 黑棋，则由人输入
+                action = human_action(env)
+            else:  # 白棋，则由agent输入
+                action = white_agent.get_action(state)
+                action = (action // cheese_border_size, action % cheese_border_size)
+
+            if not action:  # 若是无效action，跳过
+                raise TypeError("不应该出现无效action")
+            # 模型训练及保存
+            step += 1
+            next_state, reward, done, _ = env.step(action)
+            action = action[0] * cheese_border_size + action[1]
+            if done:
+                break
+            white_agent.train(state, next_state, action, -reward)  # 白棋刚好相反
+            state = next_state
+            if step % 20 == 0:
+                print('save model per 20 steps.')
+                white_agent.save()
+
+
+if __name__ == '__main__':
+    ai_vs_ai()
